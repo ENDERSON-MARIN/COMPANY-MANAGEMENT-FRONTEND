64be1fb3c64b0c0aaac033318c9cbe5da0408280
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-10-24 13:51:05 -0300

    feat(hooks): add useGetCompanyById query hook for fetching company by id

---
+++ src/hooks/queries/use-get-company-by-id.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { CompanyService } from "@/services/company.service";
+
+export const getCompanyByIdQueryKey = (id: string) => ["company", id];
+
+export function useGetCompanyById(id: string) {
+  return useQuery({
+    queryKey: getCompanyByIdQueryKey(id),
+    queryFn: () => CompanyService.getById(id),
+    enabled: !!id,
+  });
+}
